6393e8f6642fd5df8f71d27add9b14449f0698cd
= <houshaojie0619@gmail.com>
2022-05-17 16:18:06 -0500

    update

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 // const section1 = document.querySelector('#section--1');
-// const btnScrollTo = document.querySelector('.btn--scroll-to');
 
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//     e.preventDefault();
 
-
-// btnScrollTo.addEventListener('click', function (e) {
-// const s1coords = section1.getBoundingClientRect();
-// console.log(s1coords);
-
-// console.log(e.target.getBoundingClientRect());
-
-// console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-
-// console.log(
-//   'height/width viewport',
-//   document.documentElement.clientHeight,
-//   document.documentElement.clientWidth
-// );
-
-// // Scrolling
-// // window.scrollTo(
-// //   s1coords.left + window.pageXOffset,
-// //   s1coords.top + window.pageYOffset
-// // );
-
-// // window.scrollTo({
-// //   left: s1coords.left + window.pageXOffset,
-// //   top: s1coords.top + window.pageYOffset,
-// //   behavior: 'smooth',
-// // });
-
-
-
-// section1.scrollIntoView({ behavior: 'smooth' });
-// });
+//     btnScrollTo.addEventListener('click', function (e) {
+//         const s1coords = section1.getBoundingClientRect();
+//         console.log(s1coords);
+      
+//         console.log(e.target.getBoundingClientRect());
+      
+//         console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+      
+//         console.log(
+//           'height/width viewport',
+//           document.documentElement.clientHeight,
+//           document.documentElement.clientWidth
+//         );
+//         section1.scrollIntoView({ behavior: 'smooth' });
+//     });
+//     // Matching strategy
+//     if (e.target.classList.contains('nav__link')) {
+//       const id = e.target.getAttribute('href');
+//       document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//     }
+//   });
